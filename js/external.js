3937443e83eb481bc7f0ad17b71c14ccc6453c57
Thomas <trexmeyer@gmail.com>
2018-03-20 10:16:02 -0500

    pushing JS files into master branch

---
+++ js/external.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log("hello from external js");
+
+alert("Welcome to my Website");
+
+var favColor = prompt('What is your favorite color?:');
+
+if(favColor="blue" || "Blue"){
+    alert("That's my favorite color too!");
+}
